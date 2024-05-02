7a3527763a7167d5272ad5ce2013cf893efed7bf
川嶋澄生 <sumio.vantan308@gmail.com>
2024-05-03 01:49:55 +0900

    setting when reload

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,9 @@ require 'rspotify/oauth'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :spotify, ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"], scope: 'user-read-email user-top-read'
+  on_failure do |env|
+    StaticpagesController.action(:top).call(env)
+  end
 end
 
 OmniAuth.config.allowed_request_methods = [:post, :get]
