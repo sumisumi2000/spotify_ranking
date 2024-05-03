c5c009bc6c6c794de080338e636b8f715bed5b77
川嶋澄生 <sumio.vantan308@gmail.com>
2024-05-03 16:54:35 +0900

    test embed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def spotify
     spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
-    @track = spotify_user.top_tracks(limit: 1).first
+    @track = spotify_user.top_tracks(time_range: 'medium_term').second
   end
 end
