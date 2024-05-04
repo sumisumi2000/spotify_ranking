1f3f5cfb563bbd38d66df5bebfa51f636a953cf2
川嶋澄生 <sumio.vantan308@gmail.com>
2024-05-04 16:50:37 +0900

    spotify certification

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   def spotify
     spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
-    @track = spotify_user.top_tracks(time_range: 'medium_term').second
+    @track = spotify_user.top_tracks(time_range: 'long_term').first
+
+    redirect_to track_path(@track.id)
   end
 
   def track
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,10 +2,10 @@ require 'rspotify/oauth'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :spotify, ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"], scope: 'user-read-email user-top-read'
-  # 失敗した時のアクションを指定
+  # 認証失敗時のアクションを指定
   on_failure do |env|
     StaticpagesController.action(:top).call(env)
   end
 end
 
-OmniAuth.config.allowed_request_methods = [:post, :get]
+OmniAuth.config.allowed_request_methods = [:post]
