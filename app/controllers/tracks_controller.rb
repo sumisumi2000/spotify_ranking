a265c70fc2ac2883c7e72eb0c346ee23b3b8d75f
川嶋澄生 <sumio.vantan308@gmail.com>
2024-05-05 00:35:37 +0900

    create form

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,6 +1,8 @@
 class TracksController < ApplicationController
   def ranking
-    # params から曲情報を取得
-    @tracks = RSpotify::Track.find(params[:id])
+    # 保存したセッションからユーザーを検索
+    user = RSpotify::User.find(session[:spotify_user_id])
+    # フォームの値をもとにユーザーの人気曲の ID を取得
+    @track_ids = user.top_tracks(time_range: "#{params[:term]}_term", limit: params[:limit]).map{ |track| track.id }
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,6 @@
 class UsersController < ApplicationController
   def spotify
-    # 認証
-    spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
-    # よく聞く曲の ID を取得
-    @track_ids = spotify_user.top_tracks(time_range: 'long_term', limit: 20).map{ |track| track.id }
-
-    # url に曲の ID を渡す
-    redirect_to track_ranking_path(id: @track_ids)
+    # セッションにユーザーID を保存
+    session[:spotify_user_id] = RSpotify::User.new(request.env['omniauth.auth']).id
   end
 end
