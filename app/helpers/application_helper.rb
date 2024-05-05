c6feace6fafe893755b84df866dfa7354ef67a38
川嶋澄生 <sumio.vantan308@gmail.com>
2024-05-06 01:32:44 +0900

    ツイッターシェアリンク完成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,20 @@ module ApplicationHelper
       "アーティスト"
     end
   end
+
+  # シェア用の URL を生成
+  def generate_share_url(spotify_ids, term, limit, kind)
+    body = "https://twitter.com/intent/tweet?url="
+    url = "https://light-myrah-sumisumi2000.koyeb.app/share?"
+    ids = connect_spotify_ids(spotify_ids)
+    "#{body}#{url}#{ids}kind=#{kind}%26limit=#{limit}%26term=#{term}"
+  end
+
+  def connect_spotify_ids(spotify_ids)
+    ids = ""
+    spotify_ids.each do |spotify_id|
+      ids << "id%5b%5d=#{spotify_id}%26"
+    end
+    ids
+  end
 end
