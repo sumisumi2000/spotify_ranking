c3253ac394d75401d0249b686f237aa9a2b4158f
川嶋澄生 <sumio.vantan308@gmail.com>
2024-05-06 03:00:04 +0900

    シェア用のページでのエラーを修正

--- app/controllers/staticpages_controller.rb
+++ app/controllers/staticpages_controller.rb
@@ -2,7 +2,7 @@ class StaticpagesController < ApplicationController
   def top; end
 
   def share
-    @term = translate_term(params[:term])
+    @term = view_context.translate_term(params[:term])
     @limit = params[:limit]
     @kind = params[:kind]
     @spotify_ids = params[:id]
