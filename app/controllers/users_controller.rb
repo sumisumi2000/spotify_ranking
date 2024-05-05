0369f503a7ef3c815f2d41c38b3c7af9afef3dd6
川嶋澄生 <sumio.vantan308@gmail.com>
2024-05-05 18:08:49 +0900

    layout of ranking view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,8 @@ class UsersController < ApplicationController
   def spotify
     # セッションにユーザーID を保存
     session[:spotify_user_id] = RSpotify::User.new(request.env['omniauth.auth']).id
+
+    # ランキング画面にリダイレクト
+    render "tracks/ranking"
   end
 end
